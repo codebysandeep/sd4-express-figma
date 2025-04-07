6818ff0f95e7cfbacd99fbac49219a0128d8f8eb
sandeepupadhyay <supadhya@amfam.com>
2025-04-07 14:42:19 -0400

    feat: updated brand a color token

--- packages/tokens/build/ios/brand-a/tokens.h
+++ packages/tokens/build/ios/brand-a/tokens.h
@@ -10,4 +10,5 @@
 #import <UIKit/UIKit.h>
 
 #define BrandAColorBrandAColorColorSchemeLightPrimaryPrimary rgba(237, 255, 97 1)
+#define BrandAColorBrandBColorColorSchemeLightPrimaryPrimary rgba(180, 190, 200 1)
 #define BrandAMuiBrandAMuiPaletteLightTextPrimary rgba(236, 224, 255, 1)
